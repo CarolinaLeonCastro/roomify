88a227c0fecb6deaa7880f111a2d343d493ba3c8
carolina <leoncarolina35@gmail.com>
2026-02-25 15:56:58 +0100

    Add progress indicator and file name display in Upload component

--- components/Upload.tsx
+++ components/Upload.tsx
@@ -44,6 +44,14 @@ const Upload = () => {
                         )}
                     </div>
 
+                    <h3>{file.name}</h3>
+                   <div className='progress'>
+                        <div className='bar' style={{ width : `${progress}%`}}/>
+                        <p className='status-text'>
+                            {progress < 100 ? 'Analyzing Floor Plan ...' : 'Redirecting ...'}
+                        </p>
+
+                   </div>
                 </div>
             </div>
         )}
